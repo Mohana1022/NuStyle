86ad9ecc5f42c7634b76a136680295cc530ba2c7
Mohana1022 <pmohana1022@gmail.com>
2025-12-30 10:00:05 +0530

    corrected name conventions

--- checkout.js
+++ checkout.js
@@ -1,8 +1,8 @@
 let buyBtn = document.getElementById("buyBtn")
-buyBtn.addEventListener("click",()=>{
+buyBtn.addEventListener("click", () => {
   alert("Thankyou for Shopping...🛍️")
   localStorage.clear()
-  location.href="Products.html"
+  location.href = "products.html"
 })
 
 let checkout2 = document.getElementById("checkout2")
@@ -10,12 +10,12 @@ let totalPrice = document.querySelector("#checkout3>h1>span")
 
 let cart = JSON.parse(localStorage.getItem("cart"))
 console.log(cart)
-if(cart==null){
-  checkout2.innerHTML=`<p style=margin:20px>Your Cart is empty</p>`
+if (cart == null) {
+  checkout2.innerHTML = `<p style=margin:20px>Your Cart is empty</p>`
 }
 let grandTotal = 0
-cart.forEach((product)=>{
-   checkout2.innerHTML+=`
+cart.forEach((product) => {
+  checkout2.innerHTML += `
     <div class=product_container>
        <aside class=product_container1>
         <img src=${product.image} alt=${product.name} height=150 width=200>
@@ -28,6 +28,6 @@ cart.forEach((product)=>{
        </aside>
     </div>
    `
-   grandTotal+=(product.price * product.qty)
+  grandTotal += (product.price * product.qty)
   totalPrice.innerText = grandTotal.toFixed(2)
 })
